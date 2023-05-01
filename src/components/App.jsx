4dafa701cfa3b394a96b0b8a2662433f4a0b7515
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-05-01 07:34:03 +0300

    removed react from import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
 import Section from './Section';
